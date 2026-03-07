8d775f7e83bc51200b66fbbe4cdbc02d761396f6
Bien-Angelo <plazobien@gmail.com>
2026-03-07 21:38:32 +0800

    Finished 4 Fundamental Katas

---
+++ Python/BeginnerSeriesFourCockroach.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+def cockroach_speed(s):
+    return int(s*100000/(60**2))
---
+++ Python/RemoveExclamationMarks.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+"""
+
+def remove_exclamation_marks(s):
+    return s.replace("!","")
