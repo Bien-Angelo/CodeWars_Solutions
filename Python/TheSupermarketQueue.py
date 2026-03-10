16f45ee41e320361cbef9029bf83c7e00115bd7c
Bien-Angelo <plazobien@gmail.com>
2026-03-10 16:05:12 +0800

    6 Kyu

---
+++ Python/TheSupermarketQueue.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+'''
+There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the total time required for all the customers to check out!
+
+input
+customers: an array of positive integers representing the queue. Each integer represents a customer, and its value is the amount of time they require to check out.
+n: a positive integer, the number of checkout tills.
+output
+The function should return an integer, the total time required.
+'''
+def queue_time(customers, n):
+    if n > 1:
+        nList = [0] * n
+        for index_c, c in enumerate(customers):
+            shortest_line = nList.index(min(nList))
+            nList[shortest_line] = c + min(nList)
+        return max(nList)
+    else:
+        return sum(customers)
+
